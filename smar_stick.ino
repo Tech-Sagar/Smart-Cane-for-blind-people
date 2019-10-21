cf4bf6e2b491ea7e004d7878b7986be01237b3fe
Chirayu Kapoor <32087475+C-Society@users.noreply.github.com>
2019-10-21 09:05:53 +0530

    Added comment and Indentation

--- smar_stick.ino
+++ smar_stick.ino
@@ -4,31 +4,31 @@
 #define buzzer 6
 void setup()
 {
-pinMode(trigPin, OUTPUT);
-pinMode(echoPin, INPUT);
-pinMode(motor, OUTPUT);
-pinMode(buzzer,OUTPUT);
+// Defining the pins input and output
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(motor, OUTPUT);
+  pinMode(buzzer,OUTPUT);
 }
 void loop()
 {
-long duration, distance;
-digitalWrite(trigPin, LOW);
-delayMicroseconds(2);
-digitalWrite(trigPin, HIGH);
-delayMicroseconds(10);
-digitalWrite(trigPin, LOW);
-duration = pulseIn(echoPin, HIGH);
-distance = (duration/2) / 29.1;
-if (distance < 70) // Checking the distance, you can change the value
-{
-digitalWrite(motor,HIGH); // When the the distance below 70cm
-digitalWrite(buzzer,HIGH);
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance < 70) // Checking the distance, you can change the value
+  {
+    digitalWrite(motor,HIGH); // When the the distance below 70cm
+    digitalWrite(buzzer,HIGH);
+  }
+  else
+  {
+    digitalWrite(motor,LOW);// when greater than 100cm
+    digitalWrite(buzzer,LOW);
+  }
+    delay(500);
 }
-else
-{
-digitalWrite(motor,LOW);// when greater than 100cm
-digitalWrite(buzzer,LOW);
-}
-delay(500);
-}
-
